16f0f4016692df6446a14fa71808df5eca270ad9
KoahOwO <lovekm0121@naver.com>
2024-05-08 10:12:19 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+    implementation("javazoom:jlayer:1.0.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 720;
+
+    public static void main(String[] args) {
+
+        new DynamicBeat();
+    }
+}
